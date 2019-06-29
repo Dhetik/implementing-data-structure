d1feea57cace67095627d33670ff73ba68eeb35a
bitlucky <lokeshtiwari877@gmail.com>
2019-06-29 10:50:25 +0530

    Reference in C++

---
+++ Basic C and C++ conceps/5. Reference.c
@@ -0,0 +1,13 @@
+// //Reference only works in C++. 
+// // C has only two parameters. 
+// //1. pass by value.
+// //2. pass by address.
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a=10 ;
+//     int &b ;
+//     &b = a;
+//     printf("%d\n%d" , a,&b);
+// }
