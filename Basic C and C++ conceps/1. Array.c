a2dc49fa001166611f04f30761fba37ed18b44c4
bitlucky <lokeshtiwari877@gmail.com>
2019-06-29 10:48:39 +0530

    Array Basics

---
+++ Basic C and C++ conceps/1. Array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+void main(){
+    int A[5]; // initialization of array
+    int B[4] = { 1,2,3,4};
+    int i;
+    for(i=0 ; i<4 ;i++ ){
+        printf("%d" , B[i]);
+    }
+}
